3a87c7b9b51ae91e238e0a12ff3b0034c8ed3f23
yaswanth vathumilli <>
2023-02-01 14:04:09 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Content from './Content';
+
+export default function App() {
+	return (
+		<div>
+			<Header />
+			<Content />
+			<Footer />
+		</div>
+	)
+}
---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const Content = () => {
+	return (
+		<content>
+			<h1>Content</h1>
+		</content>
+	)
+}
+
+export default Content;
